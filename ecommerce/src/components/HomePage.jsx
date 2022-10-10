4bcf7688743e766bb0bf916921c002660ff3c11c
veprimmorina <65460301+veprimmorina@users.noreply.github.com>
2022-10-10 20:03:12 +0200

    sh

--- ecommerce/src/components/HomePage.jsx
+++ ecommerce/src/components/HomePage.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Footer from './Footer'
-
 import Navbar from './NavBar'
 import Products from './Products'
 import Productss from './Productss'
 import Slider from './Slider'
+import Card from 'react-bootstrap/Card';
+import ShoppingCart from './ShoppingCart'
 
 function HomePage() {
   return (
     <>
    <Navbar />
    <Slider />
+   <div className='row' style={{backgroundColor: "rgb(238, 238, 238)"}}>
+    <div className='col'>
    <Products />
    <Products />
+   </div>
+   <div className='col mt-5'>
+    <ShoppingCart />
+    </div>
+   </div>
    <Footer />
    </>
   )
--- ecommerce/src/components/Products.jsx
+++ ecommerce/src/components/Products.jsx
@@ -6,42 +6,36 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Products() {
   return (
-    <section style={{backgroundColor: "#eee"}}>
+    <section style={{backgroundColor: "#eee", width: "900px"}}>
   <div className="container py-5">
     <div className="row">
-      <div className="col-md-12 col-lg-4 mb-4 mb-lg-0">
+    <div className="col-md-6 col-lg-4 mb-4 mb-md-0">
         <div className="card">
           <div className="d-flex justify-content-between p-3">
-            <p className="lead mb-0">Today's Combo Offer</p>
-            <div
-              className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
-              style={{width: "35px", height: "35px"}}>
-              <p className="text-white mb-0 small">x4</p>
-            </div>
+            <p className="lead mb-0">Vegan Products</p> 
+              <img classname="avatar rounded-circle img-fluid" src='https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png'  style={{width: "40px", height: "25px"}} />
           </div>
           <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
             className="card-img-top" alt="Laptop" />
           <div className="card-body">
             <div className="d-flex justify-content-between">
               <p className="small"><a href="#!" className="text-muted">Laptops</a></p>
-              <p className="small text-danger"><s>$1099</s></p>
             </div>
-
             <div className="d-flex justify-content-between mb-3">
-              <h5 className="mb-0">HP Notebook</h5>
-              <h5 className="text-dark mb-0">$999</h5>
+              <h5 className="mb-0">HP Envy</h5>
+              <h5 className="text-dark mb-0">600 €</h5>
             </div>
 
             <div className="d-flex justify-content-between mb-2">
-              <p className="text-muted mb-0">Available: <span className="fw-bold">6</span></p>
+              <p className="text-muted mb-0">Available: <span className="fw-bold">7</span></p>
+             <div className='border border-dark'>
               <div className="ms-auto ">
-              <div className='border border-dark border-radius'>
-               <span className='quantity'>
+              <span className='quantity'>
                 <button><i class="bi bi-dash-circle mr-2"></i></button>
                <input id="quantity" min="1" type="number" value="1" max="1" className='text-center' />
                <button><i class="bi bi-plus-circle"></i></button>
                </span>
-               </div>
+              </div>
               </div>
             </div>
           </div>
@@ -50,34 +44,30 @@ function Products() {
       <div className="col-md-6 col-lg-4 mb-4 mb-md-0">
         <div className="card">
           <div className="d-flex justify-content-between p-3">
-            <p className="lead mb-0">Today's Combo Offer</p>
-            <div
-              className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
-              style={{width: "35px", height: "35px"}}>
-              <p className="text-white mb-0 small">x2</p>
-            </div>
+            <p className="lead mb-0">Vegan Products</p> 
+              <img classname="avatar rounded-circle img-fluid" src='https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png'  style={{width: "40px", height: "25px"}} />
           </div>
           <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/7.webp"
             className="card-img-top" alt="Laptop" />
           <div className="card-body">
             <div className="d-flex justify-content-between">
               <p className="small"><a href="#!" className="text-muted">Laptops</a></p>
-              <p className="small text-danger"><s>$1199</s></p>
             </div>
-
             <div className="d-flex justify-content-between mb-3">
               <h5 className="mb-0">HP Envy</h5>
-              <h5 className="text-dark mb-0">$1099</h5>
+              <h5 className="text-dark mb-0">600 €</h5>
             </div>
 
             <div className="d-flex justify-content-between mb-2">
               <p className="text-muted mb-0">Available: <span className="fw-bold">7</span></p>
-              <div className="ms-auto text-warning">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="far fa-star"></i>
+             <div className='border border-dark'>
+              <div className="ms-auto ">
+              <span className='quantity'>
+                <button><i class="bi bi-dash-circle mr-2"></i></button>
+               <input id="quantity" min="1" type="number" value="1" max="1" className='text-center' />
+               <button><i class="bi bi-plus-circle"></i></button>
+               </span>
+              </div>
               </div>
             </div>
           </div>
@@ -86,34 +76,30 @@ function Products() {
       <div className="col-md-6 col-lg-4 mb-4 mb-md-0">
         <div className="card">
           <div className="d-flex justify-content-between p-3">
-            <p className="lead mb-0">Today's Combo Offer</p>
-            <div
-              className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
-              style={{width: "35px", height: "35px"}}>
-              <p className="text-white mb-0 small">x3</p>
-            </div>
+            <p className="lead mb-0">Vegan Products</p> 
+              <img classname="avatar rounded-circle img-fluid" src='https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png'  style={{width: "40px", height: "25px"}} />
           </div>
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/5.webp"
-            className="card-img-top" alt="Gaming Laptop" />
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/7.webp"
+            className="card-img-top" alt="Laptop" />
           <div className="card-body">
             <div className="d-flex justify-content-between">
               <p className="small"><a href="#!" className="text-muted">Laptops</a></p>
-              <p className="small text-danger"><s>$1399</s></p>
             </div>
-
             <div className="d-flex justify-content-between mb-3">
-              <h5 className="mb-0">Toshiba B77</h5>
-              <h5 className="text-dark mb-0">$1299</h5>
+              <h5 className="mb-0">HP Envy</h5>
+              <h5 className="text-dark mb-0">600 €</h5>
             </div>
 
             <div className="d-flex justify-content-between mb-2">
-              <p className="text-muted mb-0">Available: <span className="fw-bold">5</span></p>
-              <div className="ms-auto text-warning">
-                <i className="fa fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star-half-alt"></i>
+              <p className="text-muted mb-0">Available: <span className="fw-bold">7</span></p>
+             <div className='border border-dark'>
+              <div className="ms-auto ">
+              <span className='quantity'>
+                <button><i class="bi bi-dash-circle mr-2"></i></button>
+               <input id="quantity" min="1" type="number" value="1" max="1" className='text-center' />
+               <button><i class="bi bi-plus-circle"></i></button>
+               </span>
+              </div>
               </div>
             </div>
           </div>
